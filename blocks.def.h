a2e38e4b9e6ddb4b5f58081a6253977a2185eab3
koalagang <74791721+koalagang@users.noreply.github.com>
2023-09-14 13:13:23 +0100

    Adding paths to scripts to blocks.def.h so that dwmblocks may be started before $PATH variable is set

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"VOL: ", "dwmblocks-volume",					0,		2},
-	{"BAT: ", "dwmblocks-battery",					60,		0},
-	{"", "date +'%A %d %b %H:%M'",					60,		0},
+	/*Icon*/		/*Command*/		/*Update Interval*//*Update Signal*/
+	{"VOL: ", "$HOME/.local/scripts/dwmblocks-volume",	0,		2},
+	{"BAT: ", "$HOME/.local/scripts/dwmblocks-battery",	60,		0},
+	{"", "date +'%A %d %b %H:%M'",				60,		0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
